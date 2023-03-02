837f6c7c07041df16cb0861c494fe400bfe17b55
ChristianHesberg <chri59kx@easv365.dk>
2023-03-02 13:03:15 +0100

    mqtt time slice

--- weightSensor/weightSensor.ino
+++ weightSensor/weightSensor.ino
@@ -9,25 +9,32 @@
 #define CLK  17
 #define THRESHOLD 0.1
 
+String resident = "dennis";
+
 WiFiClient espClient;
 PubSubClient client(espClient);
 HX711 scale;
 
-//const float threshold = 0.1;
-
+//timer
+hw_timer_t * timer = NULL;
+bool active = true;
+bool inBed = false;
+bool firstTimeInBed = true;
 
 
 void setup() {
   Serial.begin(9600);
   Serial.println("HX711 scale demo");
 
+  //scale
   scale.begin(DOUT, CLK);
   scale.set_scale(calibration_factor); //This value is obtained by using the SparkFun_HX711_Calibration sketch
   scale.tare(); //Assuming there is no weight on the scale at start up, reset the scale to 0
 
+  //Wifi and mqtt
   setup_wifi();
   client.setServer(mqttHost, 1883);
-  //client.setCallback(callback);
+  client.setCallback(callback);
 
   Serial.println("Readings:");
 
@@ -65,6 +72,16 @@ void callback(char* topic, byte* message, unsigned int length) {
   }
   Serial.println();
 
+  Serial.print(messageTemp);
+  if(String(topic) == "dennis/bedtime"){
+    if(messageTemp == "activate"){
+      active = true;
+    }
+    if(messageTemp == "deactivate"){
+      active = false;
+    }
+  }
+
   // Feel free to add more if statements to control more GPIOs with MQTT
 
   // If a message is received on the topic esp32/output, you check if the message is either "on" or "off". 
@@ -79,6 +96,8 @@ void reconnect() {
     // Attempt to connect
     if (client.connect("WeightSensor", flespiToken, flespiToken)) {
       Serial.println("connected");
+      //subscribe
+      client.subscribe("dennis/bedtime"); //TODO make dynamic
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -89,17 +108,50 @@ void reconnect() {
   }
 }
 
+void publishBedStatus(){
+  uint64_t timerReading = timerReadSeconds(timer);
+  String timerString = String(timerReading);
+  char* timerChars = (char*)timerString.c_str();
+  client.publish("dennis/bedStatus", timerChars, 1);
+  timerRestart(timer);
+}
+
+void listenForBedtime(){
+
+}
+
 void loop() {
-  float reading = scale.get_units();
-  if(reading > THRESHOLD){
-    String readingToString = String(reading);
-    char* readingToChars = (char*)readingToString.c_str();
-    client.publish("dennis/bedStatus", readingToChars, 0);
-    delay(2000);
-  }
   reconnect();
+  if(active){
+    float reading = scale.get_units();
+    if(reading > THRESHOLD && inBed == false){
+      if(firstTimeInBed == false){
+        publishBedStatus();
+      
+      }
+      inBed = true;
+      firstTimeInBed = false;
+      Serial.print("you're in bed");
+      timer = timerBegin(0, 80, true);
+    }
+    if(reading < THRESHOLD && inBed == true){
+      inBed = false;
+      publishBedStatus();
+      timer = timerBegin(0, 80, true);
+      Serial.print("you're out of bed");
+    }
+    
+
+
+  //uint64_t timerReading = timerReadSeconds(timer);
+  //String timerString = String(timerReading);
+  //Serial.print(timerString);
+  //Serial.println();
   //Serial.print("Reading: ");
   //Serial.print(reading, 3); //scale.get_units() returns a float
   //Serial.print(" kg"); //You can change this to kg but you'll need to refactor the calibration_factor
   //Serial.println();
+  }
+  
+  client.loop();
 }
